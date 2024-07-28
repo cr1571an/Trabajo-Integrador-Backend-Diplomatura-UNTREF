afdebd4af778667397ebd4e5971538f36ed720ba
Cristian Sanchez <sanchez.cn@hotmail.com>
2024-07-28 20:13:28 -0300

    1. Obtener todos los productos.

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+
 const connectDB = require('./database')
+const Product = require('./product')
+const port = process.env.PORT ?? 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
 
 connectDB()
+
+app.get('/products', async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.json(products);
+    } catch (error) {
+      res.status(500).send('Error al obtener los productos de informatica',error);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
+})
